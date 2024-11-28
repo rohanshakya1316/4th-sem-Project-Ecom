7ace6c619ca4edef4888aba342e772afde7a84d8
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2024-11-28 22:00:27 +0545

    products added and increment/decrement function implemented

---
+++ main.js
@@ -0,0 +1,6 @@
+import products from "./api/products.json" with {type: 'json'};
+import { showProductContainer } from "./homeProductCards.js";
+
+// Define function named 'showProductContainer' that takes an array of products as input.
+showProductContainer(products);
+   
