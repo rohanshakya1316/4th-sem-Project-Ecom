1824a0ccebd71d470356fc97fb8593d1a8c15dab
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2024-12-26 22:52:18 +0545

    login validation starting

--- Login/login.js
+++ Login/login.js
@@ -2,6 +2,18 @@ const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
 
+// Form Validation
+const form = document.querySelectorAll(".form");
+const username = document.querySelectorAll(".username");
+const email = document.querySelectorAll(".email");
+const password = document.querySelectorAll(".password");
+const password2 = document.querySelector(".password2");
+
+form.addEventListener('subimt', e => {
+    e.preventDefault();  // it cancels the event.
+
+    validationOfData();
+});
 
 registerLink.addEventListener("click", () => {
     wrapper.classList.add('active');
--- Login/login.php
+++ Login/login.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login & Registration</title>
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous"
-        referrerpolicy="no-referrer"
-    />
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link
-    <link
-    href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;700&family=Poppins:wght@200;300;400;600&family=Quicksand:wght@300;400;500;600;700&family=Urbanist:wght@300;400;600;700;800;900&display=swap"
-    rel="stylesheet"
-  />
-  <link rel="stylesheet" href="login.css">
+        href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;700&family=Poppins:wght@200;300;400;600&family=Quicksand:wght@300;400;500;600;700&family=Urbanist:wght@300;400;600;700;800;900&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="login.css">
     <script src="login.js" defer></script>
 </head>
+
 <body class="mainContainer">
     <div class="wrapper container">
         <span class="icon-close">
@@ -28,20 +24,22 @@
         <!-- Login Form Start -->
         <div class="form-box login">
             <h2>Login</h2>
-            <form action="#">
+            <form class="form" action="#">
                 <div class="input-box">
                     <span class="icon"><i class="fa-solid fa-envelope"></i></span>
-                    <input type="email" required> 
+                    <input type="email" class="email" name="email" required>
                     <label>Email</label>
+                    <div class="error">Error Here.</div>
                 </div>
                 <div class="input-box">
                     <span class="icon"><i class="fa-solid fa-lock"></i></span>
-                    <input type="password" required> 
+                    <input type="password" class="password" name="password" required>
                     <label>Password</label>
+                    <div class="error"></div>
                 </div>
                 <div class="remember-forgot">
                     <label>
-                        <input type="checkbox"> Remember me 
+                        <input type="checkbox"> Remember me
                     </label>
                     <a href="#">Forget Password?</a>
                 </div>
@@ -58,26 +56,30 @@
         <!-- Registration Form Start -->
         <div class="form-box register">
             <h2>Registration</h2>
-            <form action="#">
+            <form class="form" action="#">
                 <div class="input-box">
                     <span class="icon"><i class="fa-solid fa-user"></i></span>
-                    <input type="text" name="user" required> 
+                    <input type="text" class="username" name="username" required>
                     <label>Username</label>
-                </div>    
+                    <div class="error"></div>
+                </div>
                 <div class="input-box">
                     <span class="icon"><i class="fa-solid fa-envelope"></i></span>
-                    <input type="email" name="email" required> 
+                    <input type="email" class="email" name="email" required>
                     <label>Email</label>
+                    <div class="error"></div>
                 </div>
                 <div class="input-box">
                     <span class="icon"><i class="fa-solid fa-lock"></i></span>
-                    <input type="password" name="password" required> 
+                    <input type="password" class="password" name="password" required>
                     <label>Password</label>
+                    <div class="error"></div>                
                 </div>
                 <div class="input-box">
                     <span class="icon"><i class="fa-solid fa-lock"></i></i></span>
-                    <input type="password" name="password" required> 
+                    <input type="password" class="password2" name="password" required>
                     <label>Confirm Password</label>
+                    <div class="error"></div>
                 </div>
                 <div class="remember-forgot">
                     <label>
@@ -95,4 +97,5 @@
         <!-- Registration Form End -->
     </div>
 </body>
+
 </html>
